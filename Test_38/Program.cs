cfa65a2fd0adb2ab8cbb9d9f8e75995ae91182e7
Evgenia <evgga@mail.ru>
2023-02-25 23:56:51 +0900

    Задача 38: Разница между максимальным и минимальным элементом массива

---
+++ Test_38/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+int array = 5;
+int[] numbers = new int[array];
+
+
+void ArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+ArrayRandomNumbers(numbers);
+
+
+int min = numbers[0];
+int max = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+Console.WriteLine($"[{string.Join(",", numbers)}] -> {max - min}");
